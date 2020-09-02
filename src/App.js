0c63901d05cd1a19e3ebc802b717b63eec5f39bd
Ian Yin <yin.ian093@gmail.com>
2020-09-02 21:22:57 +1000

    revise structure

--- src/App.js
+++ src/App.js
@@ -3,30 +3,11 @@ import Header from './component/Header';
 import Home from './page/Home';
 import Footer from './component/Footer';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-      
-
-
+        <Home />
       <Footer />
     </div>
   );
